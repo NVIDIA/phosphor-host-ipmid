b81846695263788652a006b90452a69190693182
Adi Fogel <afogel@afogel-vm-01.nvidia.com>
2022-11-16 16:51:49 +0530

    Change sensor name with underscore Fixes jira https://redmine.mellanox.com/issues/3259819#change-23926076
    Signed-off-by: Adi Fogel <afogel@afogel-vm-01.nvidia.com>

--- dbus-sdr/sensorcommands.cpp
+++ dbus-sdr/sensorcommands.cpp
@@ -461,7 +461,7 @@ std::string parseSdrIdFromPath(const std::string& path)
             name.resize(FULL_RECORD_ID_STR_MAX_LENGTH);
         }
     }
-    std::replace(name.begin(), name.end(), '_', ' ');
+
     return name;
 }
 
